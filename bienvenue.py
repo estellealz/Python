b8bf1c9e71043acbcda01d5559f1bf7826a9eeb5
estelle_alz <alz.estelle@gmail.com>
2024-10-16 18:25:05 +0200

    bienvenue.py ajouter

---
+++ bienvenue.py
@@ -0,0 +1,8 @@
+# Demande le nom de l'utilisateur
+nom = input("Quel est ton nom ? ")
+
+# Demande l'âge de l'utilisateur
+age = int(input("Quel est ton âge ? "))
+
+# Affiche un message personnalisé
+print(f"Bonjour {nom} ! Tu as {age} ans.")
